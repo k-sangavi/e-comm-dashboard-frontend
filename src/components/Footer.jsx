9073aa0751f9c5224115a76d5ed5f42856ec1e18
Sangavi Karthi <sangavidev24@gmail.com>
2025-04-08 22:58:42 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h4>E-comm Dashboard@2025. All Rights reserved</h4>
+    </div>
+  );
+};
+
+export default Footer;
